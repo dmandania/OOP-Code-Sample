896b5d0b929889fe7d041fd23cdf47318700736c
dmandania <divyesh86002@gmail.com>
2017-09-06 21:06:27 +0100

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class which persists for the lifetime of the application
+/// </summary>
+public class GameController : MonoBehaviour
+{
+    /// <summary>
+    /// GameController singleton
+    /// </summary>
+    public static GameController Instance;
+
+    /// <summary>
+    /// Reference to the main camera in the scene
+    /// </summary>
+    public Camera primaryCamera;
+
+    /// <summary>
+    /// Sets the singleton
+    /// </summary>
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
